c2b34510c38d0304a88d309772b653926ca3ad50
emarfer213 <emarfer213@g.educaand.es>
2025-10-20 11:10:10 +0200

    version final clase showJuegos

--- src/main.java
+++ src/main.java
@@ -1,8 +1,4 @@
 import java.sql.SQLException;
 
 public class main {
-    public static void main(String[] args) throws SQLException {
-        GestionDeVideojuegos base = new GestionDeVideojuegos();
-        base.showJuegos();
-    }
 }
