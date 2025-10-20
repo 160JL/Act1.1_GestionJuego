32fc4ecf101ca66a069fb937f5aedbcaa730cf63
emarfer213 <emarfer213@g.educaand.es>
2025-10-20 12:30:33 +0200

    mostrarjuegos

--- src/GestionDeVideojuegos.java
+++ src/GestionDeVideojuegos.java
@@ -1,12 +1,17 @@
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class GestionDeVideojuegos {
     /**
      * @param parametro el parametro (titulo, genero, plataforma) del juego que se quiero buscar
      * @param opc la opcion a buscar (titulo, plataforma, genero)
      */
+    private static List<Videojuego> videojuegos = new ArrayList<Videojuego>();
+
     public static void buscarJuego(String parametro, int opc) {
         PreparedStatement statement;
         ResultSet rs;
@@ -55,5 +60,17 @@ public class GestionDeVideojuegos {
             System.out.println("Error: " + e.getMessage());
         }
 
+    }
+
+
+    public void showJuegos() throws SQLException {
+        Connection conexion = ConexionDB.getConnection();
+        System.out.println("Lista de juegos");
+        ResultSet rs = conexion.prepareStatement("select * from videojuegos").executeQuery();
+        while (rs.next()) {
+            Videojuego juego = new Videojuego(rs.getString(2),rs.getString(3),rs.getString(4), rs.getInt(5), rs.getInt(6), rs.getInt(1));
+            videojuegos.add(juego);
+        }
+        videojuegos.forEach(System.out::println);
     }
 }
