62656891de3949da5a8fb61428a19a4f94ec5193
DanielGS11 <dgarciasalas11@gmail.com>
2025-10-20 10:54:07 +0200

    Metodo BuscarJuego()

---
+++ src/GestionDeVideojuegos.java
@@ -0,0 +1,61 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class GestionDeVideojuegos extends Videojuego {
+    /**
+     * @param parametro el parametro (titulo, genero, plataforma) del juego que se quiero buscar
+     * @param opc la opcion a buscar (titulo, plataforma, genero)
+     */
+    public void buscarJuego(String parametro, int opc) {
+        PreparedStatement statement;
+        ResultSet rs;
+
+        try {
+            switch (opc) {
+                case 1:
+                    statement = ConexionDB.getConnection().prepareStatement("select * from videojuegos where lower(titulo) like ?");
+                    statement.setString(1, parametro.toLowerCase() + "%");
+
+                    rs = statement.executeQuery();
+
+                    while (rs.next()) {
+                        System.out.printf("*** %s ***\n- Genero: %s\n- Plataforma: %s\n- Año de Lanzamiento: %s\n- Valoración: %s\n\n",
+                                rs.getString("titulo"), rs.getString("genero"), rs.getString("plataforma"),
+                                rs.getString("anio"), rs.getString("valoracion"));
+                    }
+                    return;
+
+                case 2:
+                    statement = ConexionDB.getConnection().prepareStatement("select * from videojuegos where lower(plataforma) like ?");
+                    statement.setString(1, parametro.toLowerCase()  + "%");
+
+                    rs = statement.executeQuery();
+
+                    while (rs.next()) {
+                        System.out.printf("*** %s ***\n- Genero: %s\n- Plataforma: %s\n- Año de Lanzamiento: %s\n- Valoración: %s\n\n",
+                                rs.getString("titulo"), rs.getString("genero"), rs.getString("plataforma"),
+                                rs.getString("anio"), rs.getString("valoracion"));
+                    }
+                    return;
+
+                case 3:
+                    statement = ConexionDB.getConnection().prepareStatement("select * from videojuegos where lower(genero) like ?");
+                    statement.setString(1, parametro.toLowerCase()  + "%");
+
+                    rs = statement.executeQuery();
+
+                    while (rs.next()) {
+                        System.out.printf("*** %s ***\n- Genero: %s\n- Plataforma: %s\n- Año de Lanzamiento: %s\n- Valoración: %s\n\n",
+                                rs.getString("titulo"), rs.getString("genero"), rs.getString("plataforma"),
+                                rs.getString("anio"), rs.getString("valoracion"));
+                    }
+            }
+        } catch (SQLException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+
+    }
+}
